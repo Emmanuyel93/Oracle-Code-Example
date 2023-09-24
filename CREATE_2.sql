11c1b6f59cb689deb51b59797aa012ccddbc23d9
Emmanuyel93 <85445311+Emmanuyel93@users.noreply.github.com>
2023-09-24 22:35:22 +0530

    student table example in oracle database
    this is a sql file 
    exmaple of student table 
    #studenttabledemo
    to import this file in oracle database you can use this command "@<- file location and name -> like "@C:\All_Lab\CREATE_2.sql"
    student table automatically importe in database 
    to see this table just write : "select * fom student;"

---
+++ CREATE_2.sql
@@ -0,0 +1,84 @@
+CREATE TABLE student (
+  Id number(3),
+  Name varchar2(15),
+School_Name varchar2(25),
+Adress varchar2(20),
+City varchar2(12),
+Country varchar(10),
+Postal varchar,
+Phone number(10),
+email varchar2(30));
+
+INSERT INTO student (Id, Name,School_Name,Adress,City,Country,Postal,Phone,email) VALUES
+(1'Evan','Cap Gemini America','5 Binney St','Abbey Ward','Buckinghamshire,,'HP11 2AX',01714-737668,'evan.zigomalas@gmail.com');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(2, 'Max Ruin', 'Three', 85, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(3, 'Arnold', 'Three', 55, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(4, 'Krish Star', 'Four', 60, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(5, 'John Mike', 'Four', 60, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(6, 'Alex John', 'Four', 55, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(7, 'My John Rob', 'Five', 78, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(8, 'Asruid', 'Five', 85, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(9, 'Tes Qry', 'Six', 78, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(10, 'Big John', 'Four', 55, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(11, 'Ronald', 'Six', 89, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(12, 'Recky', 'Six', 94, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(13, 'Kty', 'Seven', 88, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(14, 'Bigy', 'Seven', 88, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(15, 'Tade Row', 'Four', 88, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(16, 'Gimmy', 'Four', 88, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(17, 'Tumyu', 'Six', 54, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(18, 'Honny', 'Five', 75, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(19, 'Tinny', 'Nine', 18, 'male')
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(20, 'Jackly', 'Nine', 65, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(21, 'Babby John', 'Four', 69, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(22, 'Reggid', 'Seven', 55, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(23, 'Herod', 'Eight', 79, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(24, 'Tiddy Now', 'Seven', 78, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(25, 'Giff Tow', 'Seven', 88, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(26, 'Crelea', 'Seven', 79, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(27, 'Big Nose', 'Three', 81, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(28, 'Rojj Base', 'Seven', 86, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(29, 'Tess Played', 'Seven', 55, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(30, 'Reppy Red', 'Six', 79, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(31, 'Marry Toeey', 'Four', 88, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(32, 'Binn Rott', 'Seven', 90, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(33, 'Kenn Rein', 'Six', 96, 'female');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(34, 'Gain Toe', 'Seven', 69, 'male');
+INSERT INTO student (Id, Name, Class, Mark, Gender) VALUES
+(35, 'Rows Noump', 'Six', 88, 'female');
+
+commit;
+
